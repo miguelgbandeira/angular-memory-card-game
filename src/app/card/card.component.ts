7419ca78cb2582934fa24e30dd1afcd8bb111cc1
Miguel Bandeira <miguelgbandeira@users.noreply.github.com>
2024-05-17 16:51:09 +0100

    install prettier

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,14 +1,11 @@
 import { Component, Input } from '@angular/core';
-import {Character} from "../character.model";
+import { Character } from '../character.model';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrl: './card.component.css'
+  styleUrl: './card.component.css',
 })
 export class CardComponent {
-
-  @Input() character: Character
-
-
+  @Input() character: Character;
 }
